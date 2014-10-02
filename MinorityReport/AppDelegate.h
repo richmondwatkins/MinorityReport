3395d7d5c24a7e26f29d3266d7a3d9c91e34b8ce
Richmond Watkins <richmondwatkins@mac.com>
2014-10-02 11:07:22 -0500

    Initial Commit

---
+++ MinorityReport/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MinorityReport
+//
+//  Created by Richmond on 10/2/14.
+//  Copyright (c) 2014 Richmond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
