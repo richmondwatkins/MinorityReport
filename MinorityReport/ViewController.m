76d6947f497a4fdc8865ad8af9bd88628d1422d1
Richmond Watkins <richmondwatkins@mac.com>
2014-10-02 13:52:41 -0500

    finished project

--- MinorityReport/ViewController.m
+++ MinorityReport/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *theFuture;
+@property (strong, nonatomic) IBOutlet UIView *thePreCogs;
+@property CGPoint originalCenter;
 
 @end
 
@@ -16,7 +19,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    self.originalCenter = self.theFuture.center;
+}
+
+-(IBAction)onDrag:(UIPanGestureRecognizer *)panGesture{
+    CGPoint point = [panGesture locationInView:self.view];
+    self.theFuture.center = point;
+
+    if (panGesture.state == UIGestureRecognizerStateEnded) {
+        [UIView animateWithDuration:4.0 animations:^{
+            self.theFuture.center = self.originalCenter;
+            self.theFuture.backgroundColor = [UIColor whiteColor];
+        }];
+    } else {
+        if(CGRectContainsPoint(self.thePreCogs.frame, point)){
+            self.theFuture.backgroundColor = [UIColor redColor];
+            self.theFuture.text = @"A ficticious and incriminating future";
+            [self.theFuture sizeToFit];
+        }
+    }
+
 }
 
 - (void)didReceiveMemoryWarning {
